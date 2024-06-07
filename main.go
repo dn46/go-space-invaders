d923f122677f54654fcec4223bc2938e999c3e03
dn46 <dn46@protonmail.ch>
2024-06-07 17:35:21 +0100

    ship movement horizontally

--- main.go
+++ main.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	raylib "github.com/gen2brain/raylib-go/raylib"
-)
-
-var (
-	SCREEN_WIDTH  int32 = 800
-	SCREEN_HEIGHT int32 = 600
+	"github.com/dn46/goRayLib/window"
 )
 
 func main() {
-	raylib.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - basic window")
-	defer raylib.CloseWindow()
-
-	raylib.SetTargetFPS(60)
-
-	for !raylib.WindowShouldClose() { // as long as the window is open
-		raylib.BeginDrawing()
-
-		raylib.ClearBackground(raylib.RayWhite)
-		raylib.DrawText("Congrats! You created your first window", 190, 200, 20, raylib.LightGray)
-
-		raylib.EndDrawing()
-	}
+	window.StartWindow()
 }
