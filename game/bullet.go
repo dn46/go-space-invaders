dccddb30f128c37109c0be459c66765c9f4ad27b
dn46 <dn46@protonmail.ch>
2024-06-08 19:03:19 +0100

    refactoring

--- game/bullet.go
+++ game/bullet.go
@@ -9,24 +9,22 @@ import (
 const shotDelay = 500 * time.Millisecond
 
 type Bullet struct {
-	Xpos        int32
-	Ypos        int32
-	velocity    int32
-	radius      float32
-	Draw        bool
-	Color       raylib.Color
-	ShouldShoot bool
+	Xpos     int32
+	Ypos     int32
+	velocity int32
+	radius   float32
+	Draw     bool
+	Color    raylib.Color
 }
 
 func NewBullet(g *Game) *Bullet {
 	return &Bullet{
-		Xpos:        g.xCoords + 50,
-		Ypos:        g.yCoords + 25,
-		velocity:    5,
-		radius:      10,
-		Draw:        true,
-		Color:       raylib.White,
-		ShouldShoot: true,
+		Xpos:     g.xCoords + 50,
+		Ypos:     g.yCoords + 25,
+		velocity: 5,
+		radius:   10,
+		Draw:     true,
+		Color:    raylib.White,
 	}
 }
 
@@ -43,7 +41,7 @@ func (g *Game) DrawBullet() {
 	// drawing the bullet
 	for index1, current_bullet := range g.Bullets {
 		if current_bullet.Draw {
-			g.Bullets[index1].Ypos = g.Bullets[index1].Ypos - current_bullet.velocity
+			g.Bullets[index1].Ypos -= current_bullet.velocity
 			raylib.DrawCircle(current_bullet.Xpos-16, current_bullet.Ypos, current_bullet.radius, current_bullet.Color)
 
 			// if the bullets are out of the screen, stop drawing them
